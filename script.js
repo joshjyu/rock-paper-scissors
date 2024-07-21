d44dce83e2e9d219e9203af28458ac4b0851f4f4
joshjyu <jjyu98@gmail.com>
2024-07-21 08:35:17 -0400

    Add comments

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 const winCondition = 5;
 let playerScore = 0;
 let computerScore = 0;
+let playerChoice;
+let compChoice;
 
 const winConditionStatement = document.querySelector(".winCondition");
 winConditionStatement.textContent = `The first to win ${winCondition} rounds is the winner.`;
 
-let playerChoice;
-let compChoice;
 let buttonWrapper = document.querySelector(".buttonWrapper");
 
+// Create one event listener around entire button wrapper
+// Use separate target for each button and assign to player's choice
 buttonWrapper.addEventListener("click", (event) => {
   let target = event.target;
 
+  // Also calculate computer's choice (0, 1, or 2) while player chooses
   compChoice = Math.floor(Math.random() * 3);
 
   switch (target.id) {
@@ -30,12 +33,16 @@ buttonWrapper.addEventListener("click", (event) => {
   }
 });
 
+// Array of string versions of choices for reference within playRound()
 const choices = ["rock", "paper", "scissors"];
+
 let result = document.querySelector("#result");
 let score = document.querySelector("#score");
 let showChoices = document.querySelector("#showChoices");
 let winnerDecl = document.createElement("p");
 let roundCount = 1;
+
+// "white-space: pre" so that new lines wrap text appropriately in playRound
 showChoices.setAttribute("style", "white-space: pre;");
 score.setAttribute("style", "white-space: pre;");
 winnerDecl.style.cssText = "font-weight: bold;";
@@ -45,6 +52,9 @@ function playRound() {
   showChoices.textContent += `Computer choice: ${choices[compChoice]}\r\n`;
   showChoices.textContent += `Player choice: ${choices[playerChoice]}`;
 
+  // Compare choices, declare results, increment scores (unless a draw)
+  // Logic just uses numbers instead of strings
+  // 0 = rock, 1 = paper, 2 = scissors
   if (playerChoice == compChoice) {
     result.textContent = "This round is a draw";
   } else if (compChoice - playerChoice == 1) {
@@ -65,16 +75,19 @@ function playRound() {
   score.textContent = `Computer score = ${computerScore}\r\n`;
   score.textContent += `Player score = ${playerScore}`;
 
+  // Once the win condition has been reached by player or comp, call checkWinner
   if (playerScore == winCondition || computerScore == winCondition)
     checkWinner();
 }
 
 function checkWinner() {
+  // First, disable buttons since game has ended
   const allButtons = document.querySelectorAll("button");
   for (let i = 0; i < allButtons.length; i++) {
     allButtons[i].disabled = true;
   }
 
+  // Declare winner and append to score declaration
   if (playerScore == winCondition) {
     winnerDecl.textContent = "You win the match";
   } else {
